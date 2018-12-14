1c86faf2610ab7bf5674a8af744defc40295d3b1
ChiaraTorta <chiaratorta@mailbox.org>
2018-12-14 20:57:58 +0100

    feat: render Google Map and Markers

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, {Component} from "react";
-import "./App.css";
-import Map from "./components/Map.js";
-import {loadGoogleMaps,
-        loadPlaces}
-        from "./utils.js";
+// import "./App.css";
+// import Map from "./components/Map.js";
+import {loadGoogleMaps, loadPlaces} from "./utils.js";
 
 export default class App extends Component {
   componentDidMount() {
     let googleMapsPromise = loadGoogleMaps();
     let foursquarePromise = loadPlaces();
 
-    Promise.all([googleMapsPromise,
-                foursquarePromise])
-    .then(values => {
+    Promise.all([googleMapsPromise, foursquarePromise]).then(values => {
+      console.log(values);
       let google = values[0];
       let venues = values[1].response.venues;
-      console.log(values);
+
       this.google = google;
       this.markers = [];
-      let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
         scrollwheel: true,
-        center: {lat: venues[0].location.lat, lng: venues[0].location.lng}
+        center: {
+          lat: venues[0].location.lat,
+          lng: venues[0].location.lng
+        }
       });
 
-      venues.map(venue => {
+      venues.forEach(venue => {
         let marker = new google.maps.Marker({
-          position: { lat: venue.location.lat, lng: venue.location.lng },
+          position: {
+            lat: venue.location.lat,
+            lng: venue.location.lng
+          },
           map: this.map,
           animation: google.maps.Animation.DROP,
           //custom parameters
@@ -34,14 +37,15 @@ export default class App extends Component {
           id: venue.id
         });
         this.markers.push(marker);
-      })
+      });
     });
   }
   render() {
     return (
-      <div className="App">
-        <div id="list-view" />
-        <div id="map" />
+      <div>
+        <main>
+          <div id="map" role="application" style={{height: "1000px"}} />
+        </main>
       </div>
     );
   }
