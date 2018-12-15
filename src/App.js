b743467f7524c8aaea0be4648da44a361478a853
ChiaraTorta <chiaratorta@mailbox.org>
2018-12-15 23:07:23 +0100

    fix: bind infowindow and marker visibility

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ export default class App extends Component {
   state: {
     query: []
   };
+
+  /*
+   *  Function to inject third party data to the component and initialize component properties
+   */
   componentDidMount() {
     let googleMapsPromise = loadGoogleMaps();
     let foursquarePromise = loadPlaces();
@@ -46,7 +50,10 @@ export default class App extends Component {
         google.maps.event.addListener(marker, "click", () => {
           marker.getAnimation() !== null
             ? marker.setAnimation(null)
-            : marker.setAnimation(google.maps.Animation.BOUNCE);
+            : marker.setAnimation(google.maps.Animation.BOUNCE) &&
+              marker.setIcon(
+                "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+              );
           setTimeout(() => {
             marker.setAnimation(null);
           }, 1500);
@@ -56,7 +63,7 @@ export default class App extends Component {
         google.maps.event.addListener(marker, "click", () => {
           this.infoWindow.setContent(marker.name);
           this.map.setCenter(marker.position);
-          this.map.setZoom(17);
+          this.map.setZoom(15);
           this.infoWindow.open(this.map, marker);
         });
 
@@ -67,25 +74,46 @@ export default class App extends Component {
     });
   }
 
+  /*
+   *  Event handler for list item click
+   */
   listItemClick = venue => {
     let marker = this.markers.filter(marker => marker.id === venue.venue.id);
+    // open infowindow
     this.infoWindow.setContent(venue.venue.name);
     this.map.setCenter(venue.venue.location);
-    this.map.setZoom(17);
+    this.map.setZoom(15);
     this.infoWindow.open(this.map, marker[0]);
+
+    // add animation
+    marker[0].getAnimation() !== null
+      ? marker[0].setAnimation(null)
+      : marker[0].setAnimation(this.google.maps.Animation.BOUNCE) &&
+        marker[0].setIcon(
+          "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+        );
+    setTimeout(() => {
+      marker[0].setAnimation(null);
+    }, 1500);
   };
 
+  /*
+   *  Function to filter list items depending on query
+   */
   filterVenues(query) {
     let f = this.venues.filter(venue =>
       venue.venue.name.toLowerCase().includes(query)
     );
-    this.setState({filteredVanues: f});
+    // set map center to first filtered value
+    f.length > 0 && this.map.setCenter(f[0].venue.location);
     this.markers.forEach(marker => {
       marker.name.toLowerCase().includes(query) === true
         ? marker.setVisible(true)
         : marker.setVisible(false);
     });
-    this.setState({query: query});
+    // close infowindow if user type a query
+    this.infoWindow.close();
+    this.setState({filteredVanues: f, query});
   }
   render() {
     return (
