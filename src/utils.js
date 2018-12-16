e05e14ab4418db53f19df976c80caebea3983a4b
ChiaraTorta <chiaratorta@mailbox.org>
2018-12-16 10:56:06 +0100

    fix: hide API

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export function loadGoogleMaps() {
     };
     // Now, Load the Google Maps API
     const script = document.createElement("script");
-    const API_KEY = "AIzaSyBFit4u5ZOCR2xgFmub3W0bN8HC5t6xms8";
+    const API_KEY = "YOUR API";
     script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
     script.async = true;
     document.body.appendChild(script);
@@ -18,9 +18,8 @@ export function loadGoogleMaps() {
 
 export function loadPlaces() {
   let latLng = "48.763441,9.271830";
-  let category = "coffee";
-  let clientId = "MB43T0UYDVLX4KHVEB4HCKB1X0J54YBZC1COCUOZA4X2XN2L";
-  let clientSecret = "LLK1OIZNPRD0W3Q5NPIP0IKWMZBAVL3BNA3QKQO4ISKPSF2A";
-  let apiURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clientSecret}&v=20180323&ll=${latLng}&query=${category}`;
+  let clientId = "YOUR CLIENT-ID";
+  let clientSecret = "YOUR CLIENT-SERVICE";
+  let apiURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=50&ll=${latLng}&query=coffee`;
   return fetch(apiURL).then(resp => resp.json());
 }
